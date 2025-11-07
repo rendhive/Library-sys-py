eb4b79f7e09b0545f5bc273f09eeb38768199ed1
Rendhive code <rendhivecode@gmail.com>
2025-11-07 22:38:25 +0700

    Add files via upload

---
+++ 17. Mencetak Pesan Kesalahan Menggunakan.py
@@ -0,0 +1,6 @@
+import sys
+
+sys.stderr.write("Ini adalah pesan kesalahan.\n")
+
+#Fungsi: Menulis pesan kesalahan ke aliran kesalahan standar.
+#Kondisi: Saat Anda ingin membedakan antara output normal dan pesan kesalahan.
