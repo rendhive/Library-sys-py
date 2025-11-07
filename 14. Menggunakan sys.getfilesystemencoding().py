e43108c9c67f47fd627a1a15ffa73b76f58acc53
Rendhive code <rendhivecode@gmail.com>
2025-11-07 22:34:50 +0700

    Add files via upload

---
+++ 14. Menggunakan sys.getfilesystemencoding().py
@@ -0,0 +1,6 @@
+import sys
+
+print("Encoding sistem file:", sys.getfilesystemencoding())
+
+#Fungsi: Menampilkan encoding yang digunakan oleh sistem file.
+#Kondisi: Ketika perlu memahami bagaimana string dikelola oleh sistem file.
